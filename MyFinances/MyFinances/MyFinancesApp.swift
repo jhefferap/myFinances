ce41902039dd5356ac4f015d5703849e1bb90d24
jefferson.valverde <>
2024-07-12 18:22:21 -0300

    First commit

---
+++ MyFinances/MyFinances/MyFinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinancesApp.swift
+//  MyFinances
+//
+//  Created by jefferson.valverde on 06/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
